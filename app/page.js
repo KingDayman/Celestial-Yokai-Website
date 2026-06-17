667297044a6eca0648c3216f10b33fca75b60dff
Dayman <parkday30@gmail.com>
2026-06-17 10:03:21 -0500

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -1,1 +1,33 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import KitsariSection from "@/components/KitsariSection";
+import YokaiCoinSection from "@/components/YokaiCoinSection";
+import BuildTheRealmSection from "@/components/BuildTheRealmSection";
+import SpeciesGrid from "@/components/SpeciesGrid";
+import EcosystemPillars from "@/components/EcosystemPillars";
+import MerchTeaser from "@/components/MerchTeaser";
+import Roadmap from "@/components/Roadmap";
+import CommunityCTA from "@/components/CommunityCTA";
+import Footer from "@/components/Footer";
 
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <KitsariSection />
+        <YokaiCoinSection />
+        <BuildTheRealmSection />
+        <SpeciesGrid />
+        <EcosystemPillars />
+        <MerchTeaser />
+        <Roadmap />
+        <CommunityCTA />
+      </main>
+      <Footer />
+    </>
+  );
+}
